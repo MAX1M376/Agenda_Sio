b272d6b9c488af15139900f511f540505dc70832
MAX1M376 <maxime.adler76@gmail.com>
2021-11-13 22:24:00 +0100

    File orga

---
+++ SIO_AgendaXamarin/SIO_AgendaXamarin/SIO_AgendaXamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SIO_AgendaXamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SIO_AgendaXamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ SIO_AgendaXamarin/SIO_AgendaXamarin/SIO_AgendaXamarin/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SIO_AgendaXamarin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SIO_AgendaXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}
