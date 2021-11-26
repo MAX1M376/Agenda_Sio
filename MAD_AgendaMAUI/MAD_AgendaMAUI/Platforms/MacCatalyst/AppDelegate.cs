52b6f7e97646c34259be33bb2f29638b7054d260
MAX1M376 <maxime.adler76@gmail.com>
2021-11-26 23:30:00 +0100

    commit MAUI

---
+++ MAD_AgendaMAUI/MAD_AgendaMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,12 @@
+﻿using Foundation;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace MAD_AgendaMAUI
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
